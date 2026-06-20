2f33f238ccb1e84c4cce45005969269200504657
Paul Pam <paul2pam@Lianguis-MacBook-Pro.local>
2026-06-20 11:39:36 -0700

    minimum viable encoder

--- rssm.py
+++ rssm.py
@@ -1,8 +1,23 @@
 import torch.nn as nn
 
 class Encoder(nn.module):
-    def __init__():
+    def __init__(self, depth, embed_dim):
         super().__init__()
+        self.in_channels = 3                #3 if RGB, 1 if grayscale
+        self.kernel_size = 4                #this and stride are just by default
+        self.stride = 2                     
+        self.embed_dim = embed_dim  
 
-    def forward():
-        return 
+        self.convs = nn.Sequential(
+                nn.Conv2d(in_channels = 3, out_channels = 32, kernel_size=4, stride = 2)
+        )        
+
+
+        
+
+
+    def forward(self, x, h): #x is the observation, and comes in a raw pixel tensor (B, C, H, W)
+        e = self.convs(x)
+
+        
+        return e
